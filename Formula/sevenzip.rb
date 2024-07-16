e14e84cf3607c2c95fdd8a5889aa9c932cc1a0d8
Bojin Li <bojinxx@gmail.com>
2024-07-16 23:56:54 +1000

    New 7-zip formula

--- Formula/sevenzip.rb
+++ Formula/sevenzip.rb
@@ -12,16 +12,6 @@ class Sevenzip < Formula
     regex(/>\s*Download\s+7-Zip\s+v?(\d+(?:\.\d+)+)\s+\([^)]+?\)/im)
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "4b05d82402484962cfe0e5e0c979bb6fc638b5866198688db847e6239059e05b"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f415f2eed462d1178abb38a0a86423bfc97e317efc3e62fd827eae639cdd398a"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9bfba9b031710ffa58befc04947dddb39f45bf2ec6e9ca700d9b666899b9981c"
-    sha256 cellar: :any_skip_relocation, sonoma:         "28ba9a09837fec00daa083d5a63a204822d44e46bff7d9ab063b760942745557"
-    sha256 cellar: :any_skip_relocation, ventura:        "07d25d1de1e5220aec2ac15dd91bf439873541a728199c5d51963dc3c7d55c73"
-    sha256 cellar: :any_skip_relocation, monterey:       "8cb24df5e3b465203966432107c87feaebd104216cc50eaf831081e76c506c2a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ada311cc2dfe95891f06f392cc11f488e4cea5ff87e834a92edf258f96456fe7"
-  end
-
   def install
     # See https://sourceforge.net/p/sevenzip/discussion/45797/thread/9c2d9061ce/#01e7
     if OS.mac?
@@ -38,7 +28,7 @@ class Sevenzip < Formula
         ["gcc", "g"]
       end
 
-      system "make", "-f", "../../cmpl_#{mk_suffix}.mak", "DISABLE_RAR_COMPRESS=0"
+      system "make", "-f", "../../cmpl_#{mk_suffix}.mak"
 
       # Cherry pick the binary manually. This should be changed to something
       # like `make install' if the upstream adds an install target.
